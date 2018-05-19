0112a333f8d0cd045e8390bab2540481620ce8a4
Ruben Alvarez <rubalv96@gmail.com>
2018-05-19 19:17:50 +0200

    Desplegando en Heroku

--- models/index.js
+++ models/index.js
@@ -3,8 +3,13 @@ const path = require('path');
 // Load ORM
 const Sequelize = require('sequelize');
 
-// To use SQLite data base:
-const sequelize = new Sequelize("sqlite:quiz.sqlite");
+// Para usar SQLite data base:
+// DATABASE_URL = sqlite:quiz.sqlite
+//Para usar Heroku Postgres data base;
+// DATABASE_URL = postgres://user:passwd@host:port/database
+
+const url = process.env.DATABASE_URL || "sqlite:quiz.sqlite";
+const sequelize = new Sequelize(url);
 
 // Import the definition of the Quiz Table from quiz.js
 sequelize.import(path.join(__dirname, 'quiz'));
